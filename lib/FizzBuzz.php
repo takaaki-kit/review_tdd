541020f420544a3fca3fb168a9d2ee088ecfe2b8
Takaaaki_z <takaaki.kit@gmail.com>
2016-07-30 16:54:37 +0900

    FizzBuzzクラスのstartメソッドの仮実装

---
+++ lib/FizzBuzz.php
@@ -0,0 +1,9 @@
+<?php
+
+class FizzBuzz{
+
+    public function start()
+    {
+        return 'Fizz';
+    }
+}
--- lib/Menu.php
+++ lib/Menu.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('FizzBuzz.php');
+
 class Menu
 {
     public function __construct($out, $in)
@@ -9,7 +11,7 @@ class Menu
     }
     public function select($mode)
     {
-        $this->out->puts('Fizz');
+        $this->out->puts((new FizzBuzz($this->in->get()))->start());
     #    if($mode == '1'){
     #        //標準入力受け付け
     #        print((new FizzBuzz())->start(/*受け付けた値*/)));
