8c49ccf464a06673016c8f39459c660e44378fb0
takaaki.konaka <takaaki.konaka@gaiax.com>
2016-08-01 13:57:34 +0900

    fizzbuzztestにて、すべての倍数の時と、どの倍数でもない時の処理の重複を排除

--- lib/FizzBuzz.php
+++ lib/FizzBuzz.php
@@ -9,17 +9,19 @@ class FizzBuzz{
 
     public function start()
     {
-        if($this->number === 3){
-            return 'Fizz';
-        }
+        if($this->number != 0){
+            if($this->number % 15 === 0){
+                return 'FizzBuzz';
+            }
 
-        if($this->number === 5){
-            return 'Buzz';
-        }
+            if($this->number % 3 === 0){
+                return 'Fizz';
+            }
 
-        if($this->number === 15){
-            return 'FizzBuzz';
+            if($this->number % 5 === 0){
+                return 'Buzz';
+            }
         }
-        return '1';
+        return "$this->number";
     }
 }
--- test/FizzBuzzTest.php
+++ test/FizzBuzzTest.php
@@ -10,7 +10,7 @@ class FizzBuzzTest extends PHPUnit_Framework_TestCase{
     public function ３の倍数の時にFizzを返す()
     {
        $this->assertEquals((new FizzBuzz(3))->start(), 'Fizz');
-       #$this->assertEquals((new FizzBuzz(6))->start(), 'Fizz');
+       $this->assertEquals((new FizzBuzz(6))->start(), 'Fizz');
     }
 
     /**
@@ -19,7 +19,7 @@ class FizzBuzzTest extends PHPUnit_Framework_TestCase{
     public function ５の倍数の時にFizzを返す()
     {
        $this->assertEquals((new FizzBuzz(5))->start(), 'Buzz');
-       #$this->assertEquals((new FizzBuzz(10))->start(), 'Buzz');
+       $this->assertEquals((new FizzBuzz(10))->start(), 'Buzz');
     }
 
     /**
@@ -28,7 +28,7 @@ class FizzBuzzTest extends PHPUnit_Framework_TestCase{
     public function １５の倍数の時にFizzBuzzを返す()
     {
        $this->assertEquals((new FizzBuzz(15))->start(), 'FizzBuzz');
-       #$this->assertEquals((new FizzBuzz(30))->start(), 'FizzBuzz');
+       $this->assertEquals((new FizzBuzz(30))->start(), 'FizzBuzz');
     }
 
     /**
@@ -37,14 +37,14 @@ class FizzBuzzTest extends PHPUnit_Framework_TestCase{
     public function いずれの倍数でもない時にその数字を返す()
     {
        $this->assertEquals((new FizzBuzz(1))->start(), '1');
-       #$this->assertEquals((new FizzBuzz(2))->start(), '2');
+       $this->assertEquals((new FizzBuzz(2))->start(), '2');
     }
 
     /**
      * @test
      */
-    #public function すべての倍数の時にその数字を返す()
-    #{
-    #   $this->assertEquals((new FizzBuzz(0))->start(), '0');
-    #}
+    public function すべての倍数の時にその数字を返す()
+    {
+       $this->assertEquals((new FizzBuzz(0))->start(), '0');
+    }
 }
